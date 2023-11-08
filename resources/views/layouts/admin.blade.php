3e6e7e6cbfe57944c2ae11b9c72a02a8e1417ec7
Fabio Costantino <fabiocostantino84@gmail.com>
2023-11-08 23:54:00 +0100

    add link Projects

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -75,13 +75,20 @@
                 <!-- Definire solo parte del menu di navigazione inizialmente per poi
         aggiungere i link necessari giorno per giorno
         -->
-                <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark text-white navbar-dark sidebar collapse">
+                <nav id="sidebarMenu"
+                    class="col-md-3 col-lg-2 d-md-block bg-dark text-white navbar-dark sidebar collapse">
                     <div class="position-sticky pt-3">
                         <ul class="nav flex-column">
                             <li class="nav-item">
 
-                                <a class="nav-link text-white {{ Route::currentRouteName() == 'dashboard' ? 'bg-secondary' : '' }}" href="{{route('dashboard')}}">
+                                <a class="nav-link text-white {{ Route::currentRouteName() == 'dashboard' ? 'bg-secondary' : '' }}"
+                                    href="{{ route('dashboard') }}">
                                     <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Dashboard
+                                </a>
+
+                                <a class="nav-link text-white {{ Route::currentRouteName() == 'admin/project' ? 'bg-secondary' : '' }}"
+                                    href="{{ route('project.index') }}">
+                                    <i class="fa-solid fa-folder-open fa-lg fa-fw"></i> Projects
                                 </a>
                             </li>
 
